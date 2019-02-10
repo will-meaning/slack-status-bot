2acc1bfcf719cca5b0a06a0005177e76a39e00e4
K <kt69.universe@gmail.com>
2019-02-10 12:43:31 +0900

    change trigger word

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bot = controller.spawn({
   token: process.env.SLACK_TOKEN
 }).startRTM();
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['check_status'], 'direct_message,direct_mention', function(bot, message) {
 
   const exclude_users = []
 
